1c935d407fa2e2b0adc4c9bc02c62c1450a59c8b
dsokol3 <dsokol3@student.touro.edu>
2026-02-01 19:28:57 -0500

    1 and 2

--- exercises/01_loops.py
+++ exercises/01_loops.py
@@ -6,3 +6,17 @@ TODO:
 4. Print the multiplication table of 5
 
 """
+
+for i in range(1, 11):
+    print(i)
+
+for i in range(0, 21, 2):
+    print(i)
+
+sum = 0
+for i in range(0, 101):
+    sum= sum + i
+print(sum)
+
+for i in range(0, 101, 5):
+    print(i)
